adebec6d260341e1aa1f57a71f4eeb720887e85d
Regan Lam <regan_lam@hotmail.com>
2021-07-01 14:28:30 -0700

    Fixed available + joined chat bug

--- src/js/actions/chats.js
+++ src/js/actions/chats.js
@@ -3,6 +3,7 @@ import db from "../db/firestore";
 
 import {
 	CHATS_CREATE_SUCCESS,
+	CHATS_JOIN_SUCCESS,
 	CHATS_FETCH_INIT,
 	CHATS_FETCH_SUCCESS,
 } from "./types";
@@ -17,12 +18,16 @@ export const fetchChats = () => async (dispatch, getState) => {
 	const joined = [];
 	const available = [];
 
+	// iterate chat
 	chatData.forEach((chat) => {
+		// iterate joinedUsers
 		for (let i = 0; i < chat.joinedUsers.length; i++) {
-			authUser.uid == chat.joinedUsers[i].id
-				? joined.push(chat)
-				: available.push(chat);
+			if (authUser.uid == chat.joinedUsers[i].id) {
+				joined.push(chat);
+				return;
+			}
 		}
+		available.push(chat);
 	});
 
 	const sortedChat = { joined, available };
@@ -47,4 +52,10 @@ export const createChat = (formData, userId) => (dispatch) => {
 			return chatId;
 		})
 		.then((chatId) => api.joinChat(userId, chatId));
+};
+
+export const joinChat = (userId, chatId) => (dispatch) => {
+	return api.joinChat(userId, chatId).then((_) => {
+		dispatch({ type: CHATS_JOIN_SUCCESS });
+	});
 };
--- src/js/components/AvailableChats.js
+++ src/js/components/AvailableChats.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { joinChat } from "../actions/chats";
 
 export default function AvailableChats({ chats }) {
+	const dispatch = useDispatch();
+	const user = useSelector(({ auth }) => auth.user);
+
+	const askForConfirmation = (chat) => {
+		const isConfirm = confirm(`Join ${chat.name}?`);
+
+		if (isConfirm) {
+			dispatch(joinChat(user.uid, chat.id));
+		}
+	};
+
 	return (
 		<div className="container-fluid">
 			<div className="row mt-3">
@@ -11,6 +25,7 @@ export default function AvailableChats({ chats }) {
 						</div>
 					</div>
 				)}
+
 				{chats.map((chat) => (
 					<div key={chat.id} className="col-lg-3 col-md-6 mb-3">
 						<div className="card">
@@ -18,7 +33,7 @@ export default function AvailableChats({ chats }) {
 								<h5 className="card-title">{chat.name}</h5>
 								<p className="card-text">{chat.description}</p>
 								<button
-									onClick={() => {}}
+									onClick={() => askForConfirmation(chat)}
 									className="btn btn-outline-primary"
 								>
 									Join Chat
